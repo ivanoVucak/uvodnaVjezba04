ff2429b6ae5a49fb2e8c1548076db6930aa75739
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-09-23 14:03:07 +0200

    Add files via upload

---
+++ fibonaccijevNiz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonaccijevNiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int brojN = 0;
+            int rezultat = 0;
+            int prethodni = 1;
+            int prethodni2 = 0;
+
+            Console.Write("Upiši broj n: ");
+            brojN = Convert.ToInt32(Console.ReadLine());
+
+            if (brojN == 0) {
+                Console.WriteLine("Rezultat tog fabonacijevog niza je 0");
+            }
+            if (brojN == 1) {
+                Console.WriteLine("Rezultat tog fabonacijevog niza je 1");
+            }
+            if (brojN > 1) {
+                for (int i = 1; i < brojN; i++) {
+                    rezultat = prethodni + prethodni2;
+                    prethodni2 = prethodni;
+                    prethodni = rezultat;
+                }
+                Console.WriteLine("Rezultat tog fabonacijevog nizda je " + rezultat);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
